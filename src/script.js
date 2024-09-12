eebea3cbcf17469947f7fc11ec1cd2ac0fec92bc
Akio <aakocherga@mail.ru>
2024-09-12 19:45:25 +0700

    Added header and head-container

---
+++ src/script.js
@@ -0,0 +1,10 @@
+function opacityHeadBackground() {
+  const container = document.querySelector(".head-container");
+  container.classList.add("opacity");
+}
+
+function load() {
+  opacityHeadBackground();
+}
+
+load();
